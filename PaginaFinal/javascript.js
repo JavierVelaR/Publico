db80b9f36670ab311620aa504ed72ec5ae3a5c08
JavierVelaR <javelo999@gmail.com>
2023-05-25 19:36:29 +0200

    terminar casi todo

--- PaginaFinal/javascript.js
+++ PaginaFinal/javascript.js
@@ -62,6 +62,17 @@ function cambiarTamanoFuente() {
     document.getElementsByTagName(p).style.fontSize=selectedValue;
 }
 
+//Intento de hacer un menú flotante de cursores personalizados
+/*function cambiarCursor(){
+    var cursores = Array["./cursores/cursorRosa.png","./cursores/cursorAjolote.png",
+    "./cursores/cursorAzul.png","./cursores/cursorZorro.png","./cursores/cursorAmarillo.png",
+    "./cursores/cursorGris.png","./cursores/cursorNegro.png.png","./cursores/cursorBlanco.png"];
+    let cursor = nextInt(prompt("Elige un cursor (0-Rosa,2-Azul,4-Amarillo,5-Gris,6-Negro):"));
+
+    localStorage.setItem("--cursor",cursores[cursor]);
+
+}*/
+
 
 // Almacenar el nombre en el almacenamiento local (persistente hasta que se cierre la pestaña o se borren los datos)
 function guardarDatos() {
